4d77be7076cc16405eaf820b3054d02f6cfe5ce0
trucknaround <98994205+trucknaround@users.noreply.github.com>
2026-04-07 19:12:42 -0400

    Conditionally render AdminPortal or PureDispatcher

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import PureDispatcher from './PureDispatcher.jsx'
+import AdminPortal from './AdminPortal.jsx'
+
+const isAdminRoute = window.location.pathname.startsWith('/admin')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PureDispatcher />
+    {isAdminRoute ? <AdminPortal /> : <PureDispatcher />}
   </React.StrictMode>,
 )
